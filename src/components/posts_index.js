a42f96d5969035b18ad5b2b23c0f29d01b60d78a
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-11 16:12:44 +0200

    Implement componentDidMount lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
+    // Fetch the data after a component rendered
+    // It's a asynchronous operation
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+    
     render () {
         return (
             <div>
@@ -10,4 +18,4 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex
+export default connect(null, { fetchPosts })(PostsIndex)
