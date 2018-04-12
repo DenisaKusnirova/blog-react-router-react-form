4cfb332a03b627daa894c20484b20800339b3e8d
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-12 22:45:40 +0200

    Add the field component for a title

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
+// Field- wired up automatically to redux, we're using
+// it to specify an input inside of the component (title, categories, contents)
+// reduxForm - it'a a function similar to the connect
+// helper from react-redux, it's going to allow redux form
+// to communicate directly from the component to the reducer
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div className="form-group">
+                <label>Title</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    // pre-generated even handlers:
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+    
     render () {
         return (
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         )
     }
 }
 
-export default PostsNew
+// form property: just a unique name of a form, we can have many forms
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew)
