8cd676289d5dc56df72b5c10a5fdecfa2f32f394
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-16 22:15:31 +0200

    Render individual post

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -4,21 +4,29 @@ import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
     componentDidMount() {
-        const { id } = this.props.match.paramas
-        this.props.fetchPost()
+        const { id } = this.props.match.params
+        this.props.fetchPost(id)
     }
 
     render() {
+        const { post } = this.props
+
+        if (!post) {
+            return <div>Loading...</div>
+        }
+
         return (
             <div>
-                posts show!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         )
     }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-    return { post: posts[ownProps.match.paramas.id] }
+    return { post: posts[ownProps.match.params.id] }
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow)
